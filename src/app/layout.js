23ecd740e4cc4fb2d99525c4d9ed52de5c938b91
Thuận Nguyễn Văn <jumrk03@gmail.com>
2025-07-23 10:42:59 +0700

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,12 @@ import { Toaster } from "react-hot-toast";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="gTRMSFUmm-DqEsxkSpkI1f_unNAypmVw9aGsnwfaNh0"
+        />
+      </head>
       <body>
         {children}
         <Toaster position="top-center" />
