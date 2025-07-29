0243b043aafb10c5a32f4e377cef7ee870bd700b
Nguyễn Văn Thuận <113599854+jumrk@users.noreply.github.com>
2025-07-29 18:36:10 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,11 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
+  // Thêm cấu hình này
+  experimental: {
+    dynamicParams: true
+  }
 };
 
 export default withNextIntl(nextConfig);
