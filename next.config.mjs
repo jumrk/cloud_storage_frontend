f0962ee6a20119bfd8d1952bbf66cd7f7f72e4fe
Thuận Nguyễn Văn <jumrk03@gmail.com>
2025-07-30 16:06:31 +0700

    fix run build

--- next.config.mjs
+++ next.config.mjs
@@ -3,17 +3,6 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-<<<<<<< HEAD
-  // Loại bỏ output: 'export' để cho phép server-side rendering
-  // output: 'export',
-  trailingSlash: true,
-  // Thêm cấu hình này
-  experimental: {
-    dynamicParams: true,
-  },
-=======
->>>>>>> b49dd680cbdea0ea433ee43f3ee8de329a56bc32
-};
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
